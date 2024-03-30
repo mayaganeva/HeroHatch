f72932e1b2b04960ba29276f0b5d01c800cb756e
Maya Ganeva <mayagganeva@gmail.com>
2024-03-30 13:26:09 +0200

    Rework in vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BaseDesires from './components/BaseDesires.vue';
+</script>
+
+<template>
+	<div class="header">
+		<h1>HeroHatch</h1>
+	</div>
+	<div class="container">
+		<BaseDesires />
+	</div>
+</template>
+
+<style>
+@import 'assets/style.css';
+</style>
